972c073c24bd3e160e3eeab53b948238f3c26cd5
Flonx <1244271534@qq.com>
2020-12-20 17:21:49 +0800

    EventExamples

---
+++ 20EventExample3.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+namespace _20EventExample3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyForm form = new MyForm();//事件的拥有者 form
+            form.Click += form.FormClicked;//事件 Click   //事件的响应者 form   //事件的处理器 FormClicked方法  //+=事件的合并
+            form.ShowDialog();
+        }
+    }
+    class MyForm : Form
+    {
+        internal void FormClicked(object sender, EventArgs e)
+        {
+            this.Text = DateTime.Now.ToString();
+        }
+    }
+}
---
+++ 20EventExample4.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+namespace _20EventExample4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            myForm form = new myForm();
+            form.ShowDialog();
+        }
+    }
+    class myForm:Form
+    {
+        private TextBox textBox;
+        private Button button;
+
+        public myForm()
+        {
+            this.textBox = new TextBox();
+            this.button = new Button();
+            this.Controls.Add(this.button);
+            this.Controls.Add(this.textBox);
+            this.button.Click += this.ButtonClicked;
+            this.button.Text = "Say Hello";
+            this.button.Top = 100;
+        }
+
+        private void ButtonClicked(object sender, EventArgs e)
+        {
+            this.textBox.Text = "Hello,World!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!";
+        }
+    }
+}
